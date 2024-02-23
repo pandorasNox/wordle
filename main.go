4ac7bb39275946c83ddb0ecddfbf5bac286401ee
pandorasnox <itspandorasnox@gmail.com>
2024-02-23 10:14:53 +0100

    add session creation

--- main.go
+++ main.go
@@ -6,20 +6,44 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 type config struct {
 	port string
 }
 
+type session struct {
+	id        string
+	expiresAt time.Time
+}
+
+type sessions []session
+
+func (ss sessions) toString() string {
+	out := ""
+	for _, s := range ss {
+		out = out + s.id + "\n"
+	}
+
+	return out
+}
+
 func main() {
 	envCfg := envConfig()
+	sessions := sessions{}
 
 	fmt.Println("hello world")
 	log.Println("hello console")
 	log.Printf("env conf: %v", envCfg)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		session := generateSession()
+		sessions = append(sessions, session)
+
+		// io.WriteString(w, fmt.Sprintf("Hello, world!\n%s", sessions.toString()))
 		io.WriteString(w, "Hello, world!\n")
 	})
 
@@ -33,4 +57,10 @@ func envConfig() config {
 	}
 
 	return config{port}
+}
+
+func generateSession() session { //todo: pass it by ref not by copy?
+	id := uuid.NewString()
+	expiresAt := time.Now().Add(120 * time.Second) // todo: 24 hour, sec now only for testing
+	return session{id, expiresAt}
 }
